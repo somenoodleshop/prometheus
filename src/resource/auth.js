47a18ddf7a7cb2c9104c6200e1dcdc59d51d3b1a
arandombit <a.random.luis@proton.me>
2025-04-28 19:46:49 -0400

    replace resource making strategy with new one

---
+++ src/resource/auth.js
@@ -0,0 +1,40 @@
+import { Configuration, FrontendApi } from '@ory/kratos-client'
+
+const {
+  KRATOS_PUBLIC_URL = 'http://localhost:4433',
+  KRATOS_ADMIN_URL = 'http://localhost:4434'
+} = process.env
+
+const kratos = new FrontendApi(new Configuration({ basePath: KRATOS_PUBLIC_URL }))
+
+const verifySession = async (req, res, next) =>
+  kratos.toSession(undefined, req.headers.cookie)
+    .then(session => {
+      req.session = session
+      next()
+    })
+    .catch(next({ status: 401, message: 'Unauthorized' }))
+
+const registration = async (req, res, next) =>
+  kratos.initializeSelfServiceRegistrationFlowForBrowsers()
+    .then(flow => res.json(flow))
+    .catch(next({ status: 500, message: 'Failed to initialize registration' }))
+
+const login = async (req, res, next) =>
+  kratos.initializeSelfServiceLoginFlowForBrowsers()
+    .then(flow => res.json(flow))
+    .catch(next({ status: 500, message: 'Failed to initialize login' }))
+
+const logout = async (req, res, next) =>
+  kratos.createSelfServiceLogoutFlowUrlForBrowsers(req.headers.cookie)
+    .then(flow => res.json(flow.data))
+    .catch(next({ status: 500, message: 'Failed to logout' }))
+
+export default [{
+  resource: '/auth',
+  behaviors: [
+    { endpoint: '/registration', method: 'get', behavior: registration },
+    { endpoint: '/login', method: 'get', behavior: login },
+    { endpoint: '/logout', method: 'get', behavior: [verifySession, logout] }
+  ]
+}]
--- src/server.js
+++ src/server.js
@@ -1,62 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
-import { Configuration, FrontendApi } from '@ory/kratos-client'
 
-const {
-  PORT = 8080,
-  KRATOS_PUBLIC_URL = 'http://localhost:4433',
-  KRATOS_ADMIN_URL = 'http://localhost:4434'
-} = process.env
+import resources from './resource.js'
+import router from './util/router.js'
+
+const { PORT = 8080 } = process.env
 
 const app = express()
-const kratos = new FrontendApi(new Configuration({ basePath: KRATOS_PUBLIC_URL }))
 
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-const verifySession = async (req, res, next) => {
-  try {
-    const session = await kratos.toSession(undefined, req.headers.cookie)
-    req.session = session
-    next()
-  } catch (err) {
-    res.status(401).json({ message: 'Unauthorized' })
-  }
-}
-
-app.get('/auth/registration', async (req, res) => {
-  try {
-    const flow = await kratos.initializeSelfServiceRegistrationFlowForBrowsers()
-    res.json(flow)
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to initialize registration' })
-  }
-})
-
-app.get('/auth/login', async (req, res) => {
-  try {
-    const flow = await kratos.initializeSelfServiceLoginFlowForBrowsers()
-    res.json(flow)
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to initialize login' })
-  }
-})
-
-auth.get('/auth/logout', verifySession, async (req, res) => {
-  try {
-    const flow = await kratos.createSelfServiceLogoutFlowUrlForBrowsers(req.headers.cookie)
-    res.json(flow.data)
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to logout' })
-  }
-})
-
-auth.get('/auth/callback', async (req, res) => {
-  
-})
 
 app.get('/', (req, res) => { res.json({ message: 'Welcome to the API' }) })
 
@@ -77,6 +33,7 @@ app.get('/profile', verifySession, async (req, res) => {
   })
 })
 
+resources.forEach(router(app))
 
 app.use((req, res) => { res.status(404).json({ message: 'Not Found' }) })
 
